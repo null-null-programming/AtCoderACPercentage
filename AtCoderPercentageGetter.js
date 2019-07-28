bea5515b024bab656b36e5670c97a4d652e3a394
null_null <nullnullprogramming@gmail.com>
2019-07-28 22:10:24 +0900

    first commit

---
+++ AtCoderPercentageGetter.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AtCoderPercentageGetter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  自分と同じくらいのレートの人々の何％がその問題を解けているかを表示する。
+// @author       null_null
+// @match        https://atcoder.jp/contests/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function () {
+    
+})();
+
+function get
+
+async function getContestStandingsData(contestScreenName) {
+
+}
