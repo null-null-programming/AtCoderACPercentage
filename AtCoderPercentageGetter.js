70d5c32dafa86c064230c558c5755161a908db73
null_null <nullnullprogramming@gmail.com>
2019-07-28 23:43:29 +0900

    add

--- AtCoderPercentageGetter.js
+++ AtCoderPercentageGetter.js
@@ -4,16 +4,33 @@
 // @version      0.1
 // @description  自分と同じくらいのレートの人々の何％がその問題を解けているかを表示する。
 // @author       null_null
-// @match        https://atcoder.jp/contests/*
+// @match        https://atcoder.jp/contests/*/standings
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
-(function () {
-    
+(async function () {
+    //今参加しているコンテスト名
+    const contestScreenName = getContestName();
+
+    const contestData = await getContestStandingsData(contestScreenName);
+
+    //コンテスト名のリスト
+    const problemNames = [];
+    contestData.TaskInfo.forEach(res => problemNames.push(res.TaskScreenName));
+
+    //コンテストの情報を辞書型に直す
+    const contestResultData = {};
+    contestData.StandingsData.forEach(res => contestResultData[res.UserName] = res);
+
+
 })();
 
-function get
+function getContestName() {
+    let contestURL = location.href;
+    let contestArray = contestURL.split('/');
+    return contestArray[contestArray.length - 2];
+}
 
 async function getContestStandingsData(contestScreenName) {
-
+    return await $.ajax(`https://atcoder.jp/contests/${contestScreenName}/standings/json`);
 }
