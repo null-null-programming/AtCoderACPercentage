394b341f752d5909861522bf4bdc74621b34616e
null_null <nullnullprogramming@gmail.com>
2019-07-29 15:56:04 +0900

    commit

--- AtCoderPercentageGetter.js
+++ AtCoderPercentageGetter.js
@@ -11,22 +11,58 @@
 (async function () {
     //今参加しているコンテスト名
     const contestScreenName = getContestName();
-
+    //参加者自身のusername
+    const userScreenName = getUserScreenName();
+    //コンテスト情報
     const contestData = await getContestStandingsData(contestScreenName);
 
     //コンテスト名のリスト
     const problemNames = [];
     contestData.TaskInfo.forEach(res => problemNames.push(res.TaskScreenName));
 
-    //コンテスト情報を辞書型に直す
+    //コンテスト情報を辞書型に直す userScreenName->Data
     const contestResultData = {};
-    contestData.StandingsData.forEach(res => contestResultData[res.UserScreenName] = res);
+    //参加回数が１5回以上のコンテスト参加者のuserScreenNameリスト
+    let contestUserName = [];
 
-    //参加者自身のusername
-    const userScreenName = getUserScreenName();
+    contestData.StandingsData.forEach(res => {
+        contestResultData[res.UserScreenName] = res
+        //自分自身は除く
+        if (res.Competitions >= 15 && res.userName !== userScreenName) contestUserName.push(res.UserScreenName);
+    });
+
     //参加者自身のRating
     const userRating = contestResultData[userScreenName].Rating;
 
+    //TODO:評価関数の洗練
+    //自身のレートとの絶対値の差が小さい順に並び替え。
+    contestUserName.sort(function (x, y) {
+        return Math.abs(userRating - contestResultData[x].Rating) - Math.abs(userRating - contestResultData[y].Rating);
+    });
+
+    //選抜者人数
+    const USER_NAM = Math.min(300, contestUserName.length);
+    //パーセントに直すときに割る定数
+    const DIV = USER_NAM / 100;
+
+    //TODO:選抜者人数の見直し
+    //自身のレートに近いUSER_NAM人の参加者を選抜
+    contestUserName = contestUserName.slice(0, USER_NAM);
+
+    //何人が解けたかを問題ごとに集計
+    let solvedPercentage = problemNames.map(problemName => {
+        let sum = 0;
+
+        //各ユーザーごとに集計
+        contestUserName.forEach(userName => {
+            if (contestResultData[userName].TaskResults[problemName]) {
+                if (contestResultData[userName].TaskResults[problemName].Score > 0) sum++;
+            }
+        });
+
+        //小数第１位までパーセントを表示
+        return Math.round(sum * 10 / DIV) / 10;
+    });
 
 })();
 
