11d181d6eee0f660ac12c777df0a742b3fbfd9ba
null_null <nullnullprogramming@gmail.com>
2019-07-30 16:41:04 +0900

    add License

--- AtCoderACPercentage.js
+++ AtCoderACPercentage.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  自分と同じくらいのレートの人々の何％がその問題を解けているかを表示する。
 // @author       null_null
+// @license      MIT
 // @match        https://atcoder.jp/contests/*/standings
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
